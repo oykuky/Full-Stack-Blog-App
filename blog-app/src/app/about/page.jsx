d852adad6140944dd4c6d5b77f0a5baef806c98c
oykuky <oykuky001@gmail.com>
2024-08-03 21:33:50 +0300

    added

--- blog-app/src/app/about/page.jsx
+++ blog-app/src/app/about/page.jsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
 import React from 'react'
+import styles from './about.module.css'
 
 function About() {
   return (
-    <div>
-      <Image src="/about.png" alt='about-img'  width={600} height={600} ></Image>
-    </div>
+    <div> className={styles.container}
+      <div>
+        <Image src="/about.png" alt='about-img'  width={600} height={600} ></Image>
+      </div>
+      <div className={styles.textContainer}>
+        <h2>About Agency</h2>
+        <h1>We create digital ideas that are bigger,bolder,braver and better.</h1>
+        <p>We create digital ideas that are bigger,bolder,braver and better.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, deleniti deserunt? Fugiat, esse perspiciatis. Repellat maiores architecto nisi assumenda expedita sequi optio, eius eveniet veritatis autem possimus doloremque nesciunt est.</p>
+      </div>
+  </div>
   )
 }
 
