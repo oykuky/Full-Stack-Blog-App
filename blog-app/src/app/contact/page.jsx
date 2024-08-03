f8884eec9bb0353aa54bf2c525fd5f4f040a4f79
oykuky <oykuky001@gmail.com>
2024-08-03 23:27:53 +0300

    contact page updated

--- blog-app/src/app/contact/page.jsx
+++ blog-app/src/app/contact/page.jsx
@@ -6,10 +6,16 @@ function Contact() {
   return (
     <div  className={styles.container}>
       <div className={styles.img}>
-        <Image src='/contact.jpg' alt="" width={650} height={650} className={styles.imgshape} ></Image>
+        <Image src='/contact.jpg' alt="" width={480} height={480} className={styles.imgshape} ></Image>
       </div>
-      <div className={styles.form}>
-
+      <div className={styles.formContainer}>
+          <form className={styles.form}>
+            <input placeholder='Name and Surname' ></input>
+            <input placeholder='Email adress' ></input>
+            <input placeholder='Phone Number (Optional)' ></input>
+            <textarea cols={60} rows={11} placeholder='Message'></textarea>
+            <button>Send</button>
+          </form>
       </div>
     </div>
   )
