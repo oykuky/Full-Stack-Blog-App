56662127cd2a08900470b5706117cbc36a22114a
oykuky <oykuky001@gmail.com>
2024-08-19 15:51:58 +0300

    updated

--- blog-app/next.config.js
+++ blog-app/next.config.js
@@ -1,8 +1,13 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.pexels.com'],
-      },
+  images:{
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: "images.pexels.com"
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- blog-app/src/lib/auth.js
+++ blog-app/src/lib/auth.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import GitHub from "next-auth/providers/Github"
 export const { handlers : { GET,POST}, auth, signIn, signOut} = NextAuth({ 
     providers:[
        GitHub ({
