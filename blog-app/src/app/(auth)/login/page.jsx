9c78684cfc846970145ab8ce3c4f0b8eca4e5d8d
oykuky <oykuky001@gmail.com>
2024-08-20 16:56:37 +0300

    bug fixed

--- blog-app/src/app/(auth)/login/page.jsx
+++ blog-app/src/app/(auth)/login/page.jsx
@@ -1,40 +1,38 @@
-
+'use client'
 import React from 'react'
 import styles from "./login.module.css";
-import { signIn } from '@/lib/auth';
-
+// import { signIn } from '@/lib/auth';
+import { signIn, signOut, useSession } from 'next-auth/react';
 // import LoginForm from '@/components/loginForm/loginForm';
 
 
 function Login() {
 
-  const handleGithubLogin = async () => {
-    "use server"
-    try {
-      await signIn("github");
-    } catch (error) {
-      console.error("GitHub authorize error", error);
-    }
-  };
-  // return (
-  //   <div className={styles.container}>
-  //     <div className={styles.wrapper}>
-  //     <form onSubmit={handleGithubLogin}>
-  //         <button type="submit" className={styles.github}>Login with Github</button>
-  //       </form>
-  //       {/* <LoginForm /> */}
-  //     </div>
-  //   </div>
-  // )
-  return (
-    <div >
-    
-      <form onSubmit={handleGithubLogin}>
-          <button type="submit" className={styles.github}>Login with Github</button>
-        </form>
+  // const handleGithubLogin = async () => {
+  //   "use server"
+  //   try {
+  //     await signIn("github");
+  //   } catch (error) {
+  //     console.error("GitHub authorize error", error);
+  //   }
+  // };
+ 
+const handleClick = () => {
+  signIn("github");
+}
   
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+      {/* <form action={handleGithubLogin}>
+          <button type="submit" className={styles.github}>Login with Github</button>
+        </form> */}
+        {/* <LoginForm /> */}
+      
+          <button onClick={handleClick} className={styles.github}>Login with Github</button>
+       
       </div>
-   
+    </div>
   )
 }
 
