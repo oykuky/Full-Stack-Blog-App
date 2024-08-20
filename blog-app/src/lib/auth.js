4913d06b58a9e93c26958b4c45cedcc2ab846639
oykuky <oykuky001@gmail.com>
2024-08-20 16:56:58 +0300

    bug fixed

--- blog-app/src/lib/auth.js
+++ blog-app/src/lib/auth.js
@@ -1,41 +1,10 @@
-// import NextAuth from "next-auth"
-// import GitHub from "next-auth/providers/Github"
-// export const { handlers : { GET,POST}, auth, signIn, signOut} = NextAuth({ 
-//     providers:[
-//        GitHub ({
-//         clientId : process.env.GITHUB_ID,
-//         clientSecret : process.env.GITHUB_SECRET,  
-//      }),
-//      ],
-//     });
-
-    
-// // import NextAuth from "next-auth";
-// // import GitHubProvider from "next-auth/providers/github";
-
-// //  const authOptions = {
-// //    providers: [
-// //      GitHubProvider({
-// //        clientId: process.env.GITHUB_ID,
-// //        clientSecret: process.env.GITHUB_SECRET,
-// //      }),
-// //    ],
-// //    secret: process.env.AUTH_SECRET,
-// //    pages: {
-// //      signIn: '/login',
-// //    },
-// //  };
-
-// // export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth(authOptions);
-import NextAuth from 'next-auth';
-import Github from 'next-auth/providers/github';
-
+import NextAuth from "next-auth"
+import GithubProvider from 'next-auth/providers/github';
 export const { handlers : { GET,POST}, auth, signIn, signOut} = NextAuth({ 
-  providers: [
-    Github({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-  ],
-});
-
+    providers:[
+      GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID ,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+     }),
+     ],
+    });
