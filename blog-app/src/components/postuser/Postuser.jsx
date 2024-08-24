1011f611b8d2346fa104757edc525f77e56a82d0
oykuky <oykuky001@gmail.com>
2024-08-24 14:18:28 +0300

    img error fixed

--- blog-app/src/components/postuser/Postuser.jsx
+++ blog-app/src/components/postuser/Postuser.jsx
@@ -8,7 +8,7 @@ const Postuser = async({userId})=> {
   const user = await getUser(userId);
   return (
          <div className={styles.container} >
-            <Image src={user.img ? user.img : "/noavatar.png"} alt='' width={50} height={50} className={styles.avatar} />
+            <Image src={user?.img ? user?.img : "/noavatar.png"} alt='' width={50} height={50} className={styles.avatar} />
               <div className={styles.box}>
               <span className={styles.title}>Author</span>
               <span className={styles.username}>{user?.username}</span>
